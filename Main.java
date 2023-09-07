b00dfede54b662fcf5852bda778fa379c4af8496
Ahmad <mahmedraza90@gmail.com>
2023-09-07 07:05:54 +0000

    Polymorphism

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String[] args){
-        Array arr = new Array();
+        List arr=new Array();
+        //Array arr = new Array();
         arr.print(Array.arrayToPass);
         System.out.println("Searching element 8 in array and outputting where its found if found:");
         System.out.println(arr.search(Array.arrayToPass, 8));
@@ -10,39 +11,19 @@ public class Main{
 }
 
 abstract class List {
-    public void print(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
-    } // End of Print Method
-
+    abstract public void print(int[] arr); 
     public void addAtStart(int element,int[] arr){};
     public void addAtEnd(int element,int[] arr){};
-    public void addAtLocation(int location,int element,int[] arr){};
+    void addAtLocation(int location,int element,int[] arr){};
     public void delAtEnd(int[] arr){};
     public void delAtStart(int[] arr){};
     public void delAtLocation(int location,int element,int[] arr){};
-
-    public int search(int[] arr,int element){
-        for(int i=0;i<arr.length;i++){  // Linear Searching
-            if (arr[i] == element){
-                return i;
-            }
-        }
-        return -1;
-    }
-    public boolean searchAtLocation(int[] arr,int element,int location){
-        if (arr[location] == element){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+    abstract public int search(int element);
+  
 }
 
 class Array extends List{
-    public static int[] arrayToPass;
+    public  int[] arrayToPass;
 
     public Array(){                         // Non parameter Constructor
         arrayToPass = new int[10];
@@ -50,4 +31,19 @@ class Array extends List{
             arrayToPass[i] = i;
         }
     }
+
+    @Override
+    public int search(int element){
+         for(int i=0;i<arrayToPass.length;i++){  // Linear Searching
+             if (arrayToPass[i] == element){
+                 return i;
+             }
+         }
+         return -1;
+     }
+    public void print(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    } // End of Print Method
 }
