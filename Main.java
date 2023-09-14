d67ea4559d6ef472b1c3ddbb1a24d9e75b660b19
Ahmad <mahmedraza90@gmail.com>
2023-09-14 20:25:11 +0500

    Final Commit

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main{
             arr = arr2;
         }
         else{
-            System.out.println("Wrong input");
+            System.out.println("Wrong input");                      // Dealing if user makes a mistake
             System.exit(1);
         }
 
